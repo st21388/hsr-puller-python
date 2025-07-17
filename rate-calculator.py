b483b3545a90649ae597575812bde1c97e2d1197
st21388 <st21388@howick.school.nz>
2025-07-17 12:12:41 +1200

    Add files via upload
    finished text-based rate calculator
    next to do:
    - utilise json files to store every pull done, then the program can calculate the rates from the json instead of using constants to determine 'pity' rate.
    - create a tkinter GUI and implement this into it.

--- rate-calculator.py
+++ rate-calculator.py
@@ -16,15 +16,10 @@ WARP_4 = 0
 WARP_5 = 0
 
 def get_rate_4_star(warp):
-    pass
-    if warp >= 10:
-        tens_digit, ones_digit = divmod(warp, 10)
-        if ones_digit == 0:
-            return 1
-        else:
-            return 0.51
+    if warp == 10: # 10 pulls is guaranteed 4 star
+        return 1.1
     else:
-        return 0.51
+        return 0.051
 
 def get_rate_5_star(warp):
     # base rate same until we reach 74th pull
@@ -40,23 +35,28 @@ while True:
     if pull_number == 1:
         WARP_4 += 1 # increment warp count of both rarities by 1
         WARP_5 += 1
-        print(WARP_4) # print the warps so we know for testing
-        print(WARP_5)
-        rate_4 = get_rate_4_star(WARP) # run 4 star rate calculator
-        print(rate_4) # print rate
-        rate_5 = get_rate_5_star(WARP) # same for 5 star
-        print(rate_5)
+        print("pity 4 star: " + str(WARP_4)) # print the warps so we know for testing
+        print("pity 5 star: " + str(WARP_5))
+        rate_4 = get_rate_4_star(WARP_4) # run 4 star rate calculator
+        print("4 star odds: " + str(rate_4)) # print rate
+        rate_5 = get_rate_5_star(WARP_5) # same for 5 star
+        print("5 star odds: " + str(rate_5))
     elif pull_number == 10:
         WARP_4 += 10 # increment warp count of both rarities by 10
         WARP_5 += 10
-        print(WARP)
-        rate_4 = get_rate_4_star(WARP)
-        print(rate_4)
-        rate_5 = get_rate_5_star(WARP)
-        print(rate_5)
+        print("pity 4 star: " + str(WARP_4))
+        print("pity 5 star: " + str(WARP_5))
+        rate_4 = get_rate_4_star(WARP_4)
+        print("4 star odds: " + str(rate_4)) # print rate
+        rate_5 = get_rate_5_star(WARP_5) # same for 5 star
+        print("5 star odds: " + str(rate_5))
     
     # do a random number from 1 - 100, check the number is lower than the rate (e.g random pulls 0.5 and the rate is 0.6 its a win)
-    if (rate_5*100) > random(0.0,100.0):
+    if (rate_5*100) > uniform(0.0,100.0) and (rate_4*100) > uniform(0.0,100.0):
         print("5 STAR WIN")
-        WARP = 0 # reset warp to 0
+        WARP_5 = 0 # reset warp to 0
+    elif (rate_4*100) > uniform(0.0,100.0):
+        print("4 STAR WIN")
+        WARP_4 = 0
     else:  # losing scenario
+        print("3 STAR")
