545f13bd3497655b9ce2e75f55c50716316fbb29
st21388 <st21388@howick.school.nz>
2025-07-23 13:40:08 +1200

    Add files via upload
    need to add pull buttons to right side panel, then add the feature to switch the banner frame when clicking on buttons, as well as the currency widget's functionality

--- main-gui.py
+++ main-gui.py
@@ -19,13 +19,17 @@ class main_frame:
         root.resizable(0,0) # make the window not resizeable
         root.configure(bg='black') # set the background
         
+        # initialising constants
+        self.currency = 0
+        self.current_banner = 1
+        
         '''LEFT SIDE PANEL'''
         left_panel = Frame(root, bg='black', width=300)
         left_panel.pack(side='left', fill='y')
         
         # banner select label
-        banner_select = Label(left_panel, text='Banner Select', bg='skyblue', width=25)
-        banner_select.pack(pady=10)
+        label_select = Label(left_panel, text='Banner Select', bg='skyblue', width=25)
+        label_select.pack(pady=10)
         
         # banner select buttons
         button_1 = Button(left_panel, text="Banner 1", width=25)
@@ -38,38 +42,58 @@ class main_frame:
         button_3.pack(pady=5)
         
         # empty space to push currency frame down
-        ## might replace this section with something more useful like a help button
         empty_frame = Frame(left_panel, bg='black')
         empty_frame.pack(pady=70)
         
         # currency input frame
-        currency_frame = Frame(left_panel, bg='skyblue', borderwidth=2)
-        currency_frame.pack(padx=20,pady=20)
+        frame_currency = Frame(left_panel, bg='skyblue', borderwidth=2)
+        frame_currency.pack(padx=20,pady=20)
         
-        currency_label = Label(currency_frame, text="Input Currency", bg='skyblue', width=25)
-        currency_label.pack(pady=10)
+        label_widget_currency = Label(frame_currency, text="Input Currency", bg='skyblue', width=25)
+        label_widget_currency.pack(pady=10)
         
-        currency_entry = Entry(currency_frame, width=25)
-        currency_entry.pack(pady=10)
+        entry_currency = Entry(frame_currency, width=25)
+        entry_currency.pack(pady=10)
         
-        currency_button_add = Button(currency_frame, text="Add")
-        currency_button_add.pack(padx=25, side="left")
+        button_add_currency = Button(frame_currency, text="Add")
+        button_add_currency.pack(padx=25, side="left")
         
-        currency_button_subtract = Button(currency_frame, text="Subtract")
-        currency_button_subtract.pack(padx=25, side="right")
+        button_subtract_currency = Button(frame_currency, text="Subtract")
+        button_subtract_currency.pack(padx=25, side="right")
         
         # details and history buttons
-        details_button = Button(left_panel, text="Details", width=12)
-        details_button.pack(side='bottom', padx=20, pady=20)
+        button_details = Button(left_panel, text="Details", width=12)
+        button_details.pack(side='bottom', padx=20, pady=20)
         
-        history_button = Button(left_panel, text="History", width=12)
-        history_button.pack(side='bottom', padx=20, pady=20)
+        button_history = Button(left_panel, text="History", width=12)
+        button_history.pack(side='bottom', padx=20, pady=20)
         
         '''RIGHT SIDE PANEL'''
-        right_panel = Frame(root, bg='lightcoral', width=1048, height=720)
-        right_panel.pack(side='left', fill='both')
+        right_panel = Frame(root, bg='lightcoral', width=980, height=720)
+        right_panel.pack(side='left', fill='both', expand=True)
         
+        # black heading bar to store banner name and current currency
+        heading_frame = Frame(right_panel, bg='black')
+        heading_frame.pack(fill='x', pady=10)
+        
+        # banner_name and varlabel currency will change depending on variables
+        label_banner = Label(heading_frame, text="Current Banner Name", fg='white', bg='black', font=("Arial", 16))
+        label_banner.pack(side='left', padx=10)
+        
+        varLabel_currency = Label(heading_frame, text='0', fg='white', bg='black')
+        varLabel_currency.pack(side='right')        
+        
+        label_currency = Label(heading_frame, text="Currency:", fg='white', bg='black')
+        label_currency.pack(side='right', padx=5)
+        
+        # current banner content
+        
+        ## NEED TO ADD: pulls up different frames depending on what banner is currently on (e.g, phainon's banner, bronya's banner, etc) 
+        
+        # pull buttons
+        pull_frame = Frame(right_panel, bg='black')
+        pull_frame.pack(side='bottom', pady=20)
 
-root = Tk()
-app = main_frame(root)
-root.mainloop()
+root = Tk() # establish the root of the window
+app = main_frame(root) # create the app object using class converter
+root.mainloop() # run/create the window
