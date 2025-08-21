b761b732e5d812c46a7d2fad4f48a244f4014f4f
st21388 <st21388@howick.school.nz>
2025-08-21 12:40:09 +1200

    c1-v3: finished
    Finished:
    - Modifying the json and the rate calculator to consider what the current banner open is.
    To-do:
    - modify every other component to read the json differently so it knows what the current banner is and gets the appropriate information

--- rate-calculator-with-json/calculator.py
+++ rate-calculator-with-json/calculator.py
@@ -18,6 +18,11 @@ rarity. E.g, [[3, 65, 8], [3, 66, 9], [4, 67, 10]] etc.
 This version uses jsons to store every individual pull. This is so that when
 implementing the GUI the reward frame can show the previous 10 pulls and so that
 the history button can show the previous 300 pulls.
+
+Component 1: Version 3:
+Edit the json file to store history for 3 different banners. The calculator
+should now instead ask for which banner the user is on, then add information to
+the history for that specific banner (banner 1, 2 or 3)
 """
 import json
 import random
@@ -46,50 +51,72 @@ def roll(rate_four, rate_five): # the current rate (account for pity) and the ty
     elif (rate_four*100) > random.uniform(0.0,100.0):
         return "4"
 
-def pull(): # function for pulling
-    data["warp_4"] += 1 # increment 4 & 5 star pity count by 1
-    data["warp_5"] += 1
-    rate_4 = get_rate_4_star(data["warp_4"]) # calculate the rates
-    rate_5 = get_rate_5_star(data["warp_5"])
+def pull(curr_banner): # function for pulling
+    data[curr_banner]["warp_4"] += 1 # increment 4 & 5 star pity count by 1
+    data[curr_banner]["warp_5"] += 1
+    rate_4 = get_rate_4_star(data[curr_banner]["warp_4"]) # calculate the rates
+    rate_5 = get_rate_5_star(data[curr_banner]["warp_5"])
     
     outcome = roll(rate_4,rate_5) # roll the dice
     if outcome == "5&4": # if both 5 and 4 star win, discard 4 star
         '''append data as (rarity, 5* pity, 4* pity)'''
-        data["history"].append([5,data["warp_5"],data["warp_4"]])
-        data["warp_5"] = 0
-        data["warp_4"] = 0
+        data[curr_banner]["history"].append([5,data[curr_banner]["warp_5"],data[curr_banner]["warp_4"]])
+        data[curr_banner]["warp_5"] = 0
+        data[curr_banner]["warp_4"] = 0
         print("5 STAR WIN!")
     elif outcome == "5": # if 5 star win, reset 5* pity not 4 star
-        data["history"].append([5,data["warp_5"],data["warp_4"]])
-        data["warp_5"] = 0
+        data[curr_banner]["history"].append([5,data[curr_banner]["warp_5"],data[curr_banner]["warp_4"]])
+        data[curr_banner]["warp_5"] = 0
         print("5 STAR WIN!")
     elif outcome == "4": # if 4 star win, reset 4* pity not 5 star
-        data["history"].append([4,data["warp_5"],data["warp_4"]])
-        data["warp_4"] = 0
+        data[curr_banner]["history"].append([4,data[curr_banner]["warp_5"],data[curr_banner]["warp_4"]])
+        data[curr_banner]["warp_4"] = 0
         print("4 STAR WIN!")
     else: # else, then 3 star was won
-        data["history"].append([3,data["warp_5"],data["warp_4"]])
+        data[curr_banner]["history"].append([3,data[curr_banner]["warp_5"],data[curr_banner]["warp_4"]])
         print("3 STAR")
     
-    if len(data["history"]) == 300:
-        data["history"].pop(0) # history can not store over 300 pulls    
+    if len(data[curr_banner]["history"]) == 300:
+        data[curr_banner]["history"].pop(0) # history can not store over 300 pulls    
 
 # open and load the list in the json into variable named data
 with open("history.json") as f:
     data = json.load(f)
 
 while True:
-    pull_number = input("Enter 1x or 10x roll: ") # get number of pulls
+    banner = input("Enter current banner: ")
+    if banner == "clear": # debug for clearing the history.json dictionary
+        data = {
+            "1": {
+                "history": [],
+                "warp_4": 0,
+                "warp_5": 0
+                },
+            "2": {
+                "history": [],
+                "warp_4": 0,
+                "warp_5": 0
+                },
+            "3": {
+                "history": [],
+                "warp_4": 0,
+                "warp_5": 0
+                },
+            "banner": 0
+        }    
+        
+    elif int(banner) > 0 and int(banner) <= 3:
+        data["banner"] = banner # change the current banner in the json
+        pull_number = input("Enter 1x or 10x roll: ") # get number of pulls
 
-    if pull_number == "1": # for single pulls
-        pull()
+        if pull_number == "1": # for single pulls
+            pull(data["banner"])
 
-    if pull_number == "10": # for 10x pulls
-        for i in range(0,10,1):
-            pull()
-
-    if pull_number == "clear": # debug for clearing the history.json dictionary
-        data = {"history": [], "warp_4": 0, "warp_5": 0}
+        if pull_number == "10": # for 10x pulls
+            for i in range(0,10,1):
+                pull(data["banner"])    
+    else:
+        print("Enter a valid banner (1, 2, or 3)")
 
 
     with open("history.json", 'w') as f:
